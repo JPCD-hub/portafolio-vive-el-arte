27cb1427f1f8a4ba267e2d4edae74a8e17ee460c
JPCD-hub <smitecufino@gmail.com>
2026-07-02 19:50:23 -0500

    resize divina sponsor logo

--- script.js
+++ script.js
@@ -483,7 +483,7 @@ const sponsors = [
   },
   {
     name: "Divina Creacion",
-    logo: "optimized/sponsors/divina-creacion-logo.jpg?v=20260703-2",
+    logo: "optimized/sponsors/divina-creacion-logo.jpg?v=20260703-3",
     description: "Casa de plantas y flores que acerca a las personas al mundo natural con plantas, flores y arreglos personalizados que transmiten vida, armonia y belleza.",
     instagram: "https://www.instagram.com/divcreat/",
     whatsapp: "https://wa.me/573003418506"
